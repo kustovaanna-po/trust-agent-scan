68f19f42d0f137445479b558197663113e16eb4e
Dmitry Alexandrovich Kozlov <dmitrii.a.kozlov@phystech.edu>
2026-03-19 10:49:21 +0300

    Update vite.config.ts TYPO

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: 'rust-agent-scan',
+  base: 'trust-agent-scan',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
