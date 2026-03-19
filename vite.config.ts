486ae25efec31d395a1e6f97c922a64f9bc20073
kustovaanna-po <kustova.anna@gmail.com>
2026-03-19 10:05:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: 'trust-agent-scan',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
